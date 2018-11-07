552504232d40f4fdf9e2b2ca2a1888ea9ae8f8e3
Nik <n.skubak@storytelling-software.com>
2018-11-08 01:53:18 +0300

    Added test for SizeList Factory

---
+++ tests/factories/profile/size_list_factory_test.py
@@ -0,0 +1,26 @@
+import unittest
+
+from vk_service.factories.profile.size_list_factory import SizeListFactory
+from vk_service.models.profile.size import Size
+
+
+class SizeListFactoryTest(unittest.TestCase):
+    def setUp(self):
+        self.factory = SizeListFactory()
+        self.size = {
+            'src': 'http://example.com/m.jpg', 'width': 130, 'height': 87,
+            'type': 'm'
+        }
+        self.values = [self.size.copy()]
+
+    def test_factory_build(self):
+        values = self.values.copy()
+        sizes = self.factory.build(values)
+        self.assertIsInstance(sizes, list)
+        for i, size in enumerate(sizes):
+            self.assertIsInstance(size, Size)
+            self.assertEqual(size.__dict__, values[i])
+
+
+if __name__ == '__main__':
+    unittest.main()
