957654ed844eb23ad3b31f5481fbeed06fc18825
Nik <n.skubak@storytelling-software.com>
2018-11-07 20:28:53 +0300

    In the models.common added model CropPhoto

--- models/common/__init__.py
+++ models/common/__init__.py
@@ -2,3 +2,4 @@ from .city import City
 from .common import Common
 from .counter import Counter
 from .country import Country
+from .crop_photo import CropPhoto
---
+++ models/common/crop_photo.py
@@ -0,0 +1,6 @@
+from abc import ABC
+from .common import Common
+
+
+class CropPhoto(Common, ABC):
+    pass
