56129807668f0c9f4dfe9224222ee06b6a86079f
Nik <n.skubak@storytelling-software.com>
2018-11-08 01:51:05 +0300

    Added test for Rect Factory

---
+++ tests/factories/profile/rect_factory_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from vk_service.factories.profile.rect_factory import RectFactory
+from vk_service.models.profile.rect import Rect
+
+
+class RectFactoryTest(unittest.TestCase):
+    def setUp(self):
+        self.factory = RectFactory()
+        self.values = {'x': 1, 'y': 1, 'x2': 99, 'y2': 99}
+
+    def test_factory_build(self):
+        values = self.values.copy()
+        rect = self.factory.build(values)
+        self.assertIsInstance(rect, Rect)
+        self.assertEqual(rect.__dict__, values)
+
+
+if __name__ == '__main__':
+    unittest.main()
