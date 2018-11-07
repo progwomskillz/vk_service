ca729145500651fa78236551765ab5d4ce3f705b
Nik <n.skubak@storytelling-software.com>
2018-11-07 23:29:02 +0300

    Added Photo model

---
+++ models/group/photo.py
@@ -0,0 +1,10 @@
+from vk_service.models.common.photo import Photo
+from vk_service.factories.group.size_list_factory import SizeListFactory
+
+
+class Photo(Photo):
+    def __init__(self, values):
+        factories = {
+            'sizes': SizeListFactory()
+        }
+        super(Photo, self).__init__(values, factories)
