b4e3da826b8b5ad18194ca5103c891783cae0f63
Nik <n.skubak@storytelling-software.com>
2018-11-07 16:26:16 +0300

    Added methods to the VKService

--- vk_service.py
+++ vk_service.py
@@ -2,4 +2,20 @@ from social_network import SocialNetworkBase
 
 
 class VKService(SocialNetworkBase):
-    pass
+    def authenticate(self):
+        pass
+
+    def get_feed(self):
+        pass
+
+    def create_post(self):
+        pass
+
+    def get_profile(self):
+        pass
+
+    def like_add(self):
+        pass
+
+    def like_delete(self):
+        pass
