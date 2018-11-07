e4d07c38b55b7c1564afe6c2b4874936732004a1
Nik <n.skubak@storytelling-software.com>
2018-11-07 20:25:12 +0300

    In the models.common added model Counter

--- models/common/__init__.py
+++ models/common/__init__.py
@@ -1,2 +1,3 @@
 from .city import City
 from .common import Common
+from .counter import Counter
---
+++ models/common/counter.py
@@ -0,0 +1,6 @@
+from abc import ABC
+from .common import Common
+
+
+class Counter(Common, ABC):
+    pass
