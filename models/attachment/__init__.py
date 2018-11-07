13dde625b7d2b8e4f88a641c4763fdb92654c201
Nik <n.skubak@storytelling-software.com>
2018-11-07 16:42:49 +0300

    Added Action model to the  models.attachment module

--- models/attachment/__init__.py
+++ models/attachment/__init__.py
@@ -1,1 +1,2 @@
+from vk_service.models.attachment.action import Action
 from vk_service.models.attachment.attachment import Attachment
---
+++ models/attachment/action.py
@@ -0,0 +1,4 @@
+from vk_service.models.attachment import Attachment
+
+class Action(Attachment):
+    pass
