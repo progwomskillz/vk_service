bb45ae91c16cbf8a0dc5119fe3af434c914101f6
Nik <n.skubak@storytelling-software.com>
2018-11-07 23:01:25 +0300

    Changed Market model in the models.attachment

--- models/attachment/market.py
+++ models/attachment/market.py
@@ -1,11 +1,11 @@
-from .attachment import Attachment
+from vk_service.models.common.market import Market
 from vk_service.factories.attachment.price_factory import PriceFactory
 from vk_service.factories.attachment.category_factory import CategoryFactory
 from vk_service.factories.attachment.photo_list_factory import PhotoListFactory
 from vk_service.factories.attachment.like_factory import LikeFactory
 
 
-class Market(Attachment):
+class Market(Market):
     def __init__(self, values):
         factories = {
             'price': PriceFactory(),
