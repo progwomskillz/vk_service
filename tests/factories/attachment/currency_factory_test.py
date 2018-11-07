0e9a8f3810b233952e76c69e1c7b275b4041ec76
Nik <n.skubak@storytelling-software.com>
2018-11-07 21:18:02 +0300

    Added test for Currency Factory

---
+++ tests/factories/attachment/currency_factory_test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from vk_service.factories.attachment.currency_factory import CurrencyFactory
+from vk_service.models.attachment.currency import Currency
+
+
+class CurrencyFactoryTest(unittest.TestCase):
+    def setUp(self):
+        self.factory = CurrencyFactory()
+        self.values = {'id': 1, 'name': 'RUB'}
+
+    def test_factory_build(self):
+        values = self.values.copy()
+        currency = self.factory.build(values)
+        self.assertIsInstance(currency, Currency)
+        self.assertEqual(currency.__dict__, values)
+
+
+if __name__ == '__main__':
+    unittest.main()
