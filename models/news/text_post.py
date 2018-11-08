10ab855be837ea20b4e5fe7adef399724b5e51a7
Nik <n.skubak@storytelling-software.com>
2018-11-08 16:07:46 +0300

    Added PostText model

---
+++ models/news/text_post.py
@@ -0,0 +1,22 @@
+from .post import Post
+from vk_service.factories.news.comment_factory import CommentFactory
+from vk_service.factories.news.like_factory import LikeFactory
+from vk_service.factories.news.repost_factory import RepostFactory
+from vk_service.factories.attachment.attachment_list_factory import AttachmentListFactory
+from vk_service.factories.news.geo_factory import GeoFactory
+from vk_service.factories.news.view_factory import ViewFactory
+from vk_service.factories.news.post_source_factory import PostSourceFactory
+
+
+class PostText(Post):
+    def __init__(self, post, profiles, groups):
+        factories = {
+            'comments': CommentFactory(),
+            'likes': LikeFactory(),
+            'reposts': RepostFactory(),
+            'attachments': AttachmentListFactory(),
+            'geo': GeoFactory(),
+            'views': ViewFactory(),
+            'post_source': PostSourceFactory()
+        }
+        super(PostText, self).__init__(post, factories, profiles, groups)
