216bdb9862c6aedc16d6d10c2774489384c4d967
Nik <n.skubak@storytelling-software.com>
2018-11-08 18:31:08 +0300

    Fixed Photo model

--- models/news/photo.py
+++ models/news/photo.py
@@ -1,10 +1,16 @@
 from vk_service.models.common.photo import Photo
 from vk_service.factories.news.size_list_factory import SizeListFactory
+from vk_service.factories.news.like_factory import LikeFactory
+from vk_service.factories.news.repost_factory import RepostFactory
+from vk_service.factories.news.comment_factory import CommentFactory
 
 
 class Photo(Photo):
     def __init__(self, values):
         factories = {
-            'sizes': SizeListFactory()
+            'sizes': SizeListFactory(),
+            'likes': LikeFactory(),
+            'reposts': RepostFactory(),
+            'comments': CommentFactory()
         }
         super(Photo, self).__init__(values, factories)
--- tests/factories/news/photo_list_factory_test.py
+++ tests/factories/news/photo_list_factory_test.py
@@ -3,24 +3,35 @@ import unittest
 from vk_service.factories.news.photo_list_factory import PhotoListFactory
 from vk_service.models.news.photo import Photo
 from vk_service.models.news.size import Size
+from vk_service.models.news.like import Like
+from vk_service.models.news.repost import Repost
+from vk_service.models.news.comment import Comment
 
 
 class PhotoListFactoryTest(unittest.TestCase):
     def setUp(self):
         self.factory = PhotoListFactory()
         self.size = {
-            'src': 'http://example.com/m.jpg', 'width': 130,
-            'height': 87, 'type': 'm'
+            'src': 'http://example.com/m.jpg', 'width': 130, 'height': 87,
+            'type': 'm'
         }
         self.sizes = [self.size.copy(), self.size.copy()]
+        self.like = {'user_likes': 0, 'count': 5}
+        self.repost = {'count': 5, 'user_reposted': 0}
+        self.comment = {'count': 5}
         self.photo = {
-            'id': 3, 'album_id': 2, 'owner_id': 53, 'user_id': 53, 'text': 'D',
-            'date': 25748559, 'sizes': self.sizes.copy(), 'width': 153,
-            'height': 154
+            'id': 53, 'album_id': -7, 'owner_id': -31412, 'user_id': 100,
+            'sizes': self.sizes.copy(), 'text': 'None', 'date': 52562326,
+            'post_id': 515, 'access_key': '8541511asd', 'likes': self.like.copy(),
+            'reposts': self.repost.copy(), 'comments': self.comment.copy(),
+            'can_comment': 1, 'can_repost': 1
         }
         self.values = [self.photo.copy(), self.photo.copy()]
         self.submodels = {
-            'sizes': Size
+            'sizes': Size,
+            'likes': Like,
+            'reposts': Repost,
+            'comments': Comment
         }
 
     def test_factory_build(self):
@@ -32,8 +43,11 @@ class PhotoListFactoryTest(unittest.TestCase):
             for key in self.submodels:
                 values[i][key] = photo.__dict__[key]
             for key in self.submodels:
-                for item in photo.__dict__[key]:
-                    self.assertIsInstance(item, self.submodels[key])
+                if isinstance(photo.__dict__[key], list):
+                    for item in photo.__dict__[key]:
+                        self.assertIsInstance(item, self.submodels[key])
+                else:
+                    self.assertIsInstance(photo.__dict__[key], self.submodels[key])
             self.assertEqual(photo.__dict__, values[i])
 
 
