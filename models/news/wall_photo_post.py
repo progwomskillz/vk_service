8fd3fbca737269010083a4b197e259b6e69803be
Nik <n.skubak@storytelling-software.com>
2018-11-08 18:32:56 +0300

    Added Wapp Photo Post model

---
+++ models/news/wall_photo_post.py
@@ -0,0 +1,10 @@
+from .post import Post
+from vk_service.factories.news.photos_factory import PhotosFactory
+
+
+class TextPost(Post):
+    def __init__(self, post, profiles, groups):
+        factories = {
+            'photos': PhotosFactory()
+        }
+        super(TextPost, self).__init__(post, profiles, groups, factories)
